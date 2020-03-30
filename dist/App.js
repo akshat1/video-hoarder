2e8b82be6c6a55b9494128931c826910756979a4
Akshat Sharma <akshat.sh@gmail.com>
2020-03-29 23:12:12 -0700

    Looks like I committed dist directory at some point. Let's remove it.

--- dist/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { getStore } from './redux';
-import InputForm from './components/InputForm';
-export default (() => /*#__PURE__*/React.createElement(Provider, {
-  store: getStore()
-}, /*#__PURE__*/React.createElement("div", {
-  id: "App"
-}, /*#__PURE__*/React.createElement(InputForm, null))));
--- dist/components/InputForm.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-var InputForm = () => /*#__PURE__*/React.createElement("h1", null, "Input Form");
-
-export default InputForm;
