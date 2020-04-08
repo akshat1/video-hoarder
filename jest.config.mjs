1ebebc97b3a7e9a71f9a3d68a96cd224e8f18b1a
Akshat Sharma <akshat.sh@gmail.com>
2020-04-08 01:18:53 -0700

    Exclude stories from test coverage.

--- jest.config.mjs
+++ jest.config.mjs
@@ -84,7 +84,7 @@ export default {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: [".*\\.story\\.jsx"],
 
   // Activates notifications for test results
   // notify: false,
