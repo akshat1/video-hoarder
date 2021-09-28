f13dd5ca33a855dbf6ae4944da84cdd944db84e6
Akshat Sharma <akshat.sh@gmail.com>
2021-09-27 23:30:04 -0700

    Layout tweak.

--- src/client/DownloadOptions.tsx
+++ src/client/DownloadOptions.tsx
@@ -1,13 +1,15 @@
 import { YTMetadata } from "../model/YouTube";
 import { FormatSelector } from "./FormatSelector";
 import { Thumbnail } from "./Thumbnail";
-import { Container, Grid, Theme, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import ansiHTML from "ansi-to-html";
 import React, { ChangeEvent, FunctionComponent, useState } from "react";
 
-const useStyle = makeStyles((theme: Theme) => ({
-  root: {},
+const useStyle = makeStyles(() => ({
+  root: {
+    alignItems: "start",
+  },
   thumbnail: {
     maxWidth: "100%",
   },
@@ -43,35 +45,33 @@ export const DownloadOptions:FunctionComponent<DownloadOptionsProps> = (props) =
   console.log("metadata:", metadata);
 
   return (
-    <Container className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Typography variant="h4">{metadata.title}</Typography>
+    <Grid container spacing={3} className={classes.root}>
+      <Grid item xs={12}>
+        <Typography variant="h4">{metadata.title}</Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Thumbnail
+          thumbnails={metadata.thumbnails}
+          fallback={metadata.thumbnail}
+          className={classes.thumbnail}
+        />
+      </Grid>
+      <Grid container item xs={6} spacing={3} className={classes.options}>
+        <Grid item>
+          <Typography variant="h6">Download format</Typography>
         </Grid>
-        <Grid item xs={12}>
-          <Thumbnail
-            thumbnails={metadata.thumbnails}
-            fallback={metadata.thumbnail}
-            className={classes.thumbnail}
+        <Grid>
+          <FormatSelector
+            formats={formats}
+            value={format?.formatId}
+            onChange={onFormatChange}
           />
         </Grid>
-        <Grid container item xs={12} spacing={3} className={classes.options}>
-          <Grid item>
-            <Typography variant="h6">Download format</Typography>
-          </Grid>
-          <Grid>
-            <FormatSelector
-              formats={formats}
-              value={format?.formatId}
-              onChange={onFormatChange}
-            />
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h5">Description</Typography>
-          <Typography dangerouslySetInnerHTML={toHTML(metadata.description)} />
-        </Grid>
       </Grid>
-    </Container>
+      <Grid item xs={12}>
+        <Typography variant="h5">Description</Typography>
+        <Typography dangerouslySetInnerHTML={toHTML(metadata.description)} />
+      </Grid>
+    </Grid>
   );
 };
