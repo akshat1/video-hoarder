d9781f47f13e916e916f74535beae63d17ec0e76
Akshat Sharma <akshat.sh@gmail.com>
2021-10-03 01:48:46 -0700

    More stubbing of the item list.

--- src/client/ItemList.tsx
+++ src/client/ItemList.tsx
@@ -1,7 +1,11 @@
+import { Grid } from "@material-ui/core";
 import React, { FunctionComponent } from "react";
 
 export const ItemList:FunctionComponent = () => {
   return (
-    <h1>Items</h1>
+    <Grid container>
+      <Grid xs={12}>Filter</Grid>
+      <Grid xs={12}>Items</Grid>
+    </Grid>
   );
 };
